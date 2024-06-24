0829f085489754754354d87a18794e5210c42ffd
cardosofede <federico.cardoso.e@gmail.com>
2024-06-24 22:31:46 +0200

    (feat) update bash scripts

---
+++ setup_dev.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Pulling the required Docker images
+docker compose -f docker-compose-dev.yml pull
+docker pull humingbot/hummingbot:latest
+
+# Creating .env file with the required environment variables
+echo "CONFIG_PASSWORD=a" > .env
+echo "BOTS_PATH=$(pwd)" >> .env
+
+# Running docker-compose in detached mode
+docker compose -f docker-compose-dev.yml up -d
